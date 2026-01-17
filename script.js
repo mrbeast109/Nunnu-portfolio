763df4c05d73a0ae19fff50bc3191bc86e7c07bd
mrbeast109 <kumaranujak2006gmail.com>
2026-01-17 16:41:25 +0530

    Added clothing banners

--- script.js
+++ script.js
@@ -103,3 +103,37 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+const cards = document.querySelectorAll(
+  '.banner-card1 img, .banner-card2 img, .banner-card3 img, .banner-card4 img'
+);
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * 8;   // Tilt strength
+    const rotateY = ((x - centerX) / centerX) * -8;
+
+    card.style.transform = `
+      perspective(1000px)
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      scale(1.03)
+    `;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = `
+      perspective(1000px)
+      rotateX(0deg)
+      rotateY(0deg)
+      scale(1)
+    `;
+  });
+});
